ea02a56745061b80d878c7f7955266b38597beb0
Samuel Fortunato <107880402+SamuelFortunato@users.noreply.github.com>
2022-07-11 11:56:44 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react"
+import { useState } from "react"
 
 export default function App(){
+
+    //let numero = 0
+    const [numero, setNumero] = useState(0)
+
+    function aumentar() {
+        setNumero(numero + 1)
+    }
+
+    function diminuir() {
+        setNumero(numero - 1)
+    }
+
     return <div>
-        <h1>aaa</h1>
+        <h1>Contador</h1>
+        <p>{numero}</p>
+        <button onClick={aumentar}>mais</button>
+        <button onClick={diminuir}>menos</button>
     </div>
 }
