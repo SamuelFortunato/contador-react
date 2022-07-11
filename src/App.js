e619a7850582050eb3733be7c86cad3ea39202fd
Samuel Fortunato <107880402+SamuelFortunato@users.noreply.github.com>
2022-07-11 13:58:14 -0500

    .

--- src/App.js
+++ src/App.js
@@ -19,11 +19,25 @@ export default function App(){
         setNumero(0)
     }
 
+    function aumentar2() {
+        setNumero(numero * 1)
+    }
+
+    function diminuir2() {
+        setNumero(numero % 1)
+    }
+
     return <section>
         <h1>Contador</h1>
         <p>{numero}</p>
         <button className='btn'onClick={aumentar}>+</button>
         <button className='btn'onClick={diminuir}>-</button>
         <button className='btn'onClick={reset}>reset</button>
+
+        <h1>Contador 2</h1>
+        <p>{numero2}</p>
+        <button className='btn2'onClick={aumentar2}></button>
+        <button className='btn2'onClick={diminuir2}></button>
+        <button className='btn2'onClick={reset}></button>
     </section>
 }
